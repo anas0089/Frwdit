ff16d373909416bc9e55ba464e0875f98f7c65d7
anas0089 <85271497+anas0089@users.noreply.github.com>
2021-08-19 14:49:30 +0530

    Update running.py

--- plugins/running.py
+++ plugins/running.py
@@ -63,7 +63,7 @@ async def run(bot, message):
             pass
    # await m.delete()
     buttons = [[
-        InlineKeyboardButton('📜 Support Group', url='https://t.me/DxHelpDesk')
+        InlineKeyboardButton('📜 Support Group', url='https://t.me/tgbotschat')
     ]] 
     reply_markup = InlineKeyboardMarkup(buttons)
     await m.edit(

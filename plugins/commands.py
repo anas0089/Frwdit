805fad8b93ec8c206e3db15027df57cbb121535c
anas0089 <85271497+anas0089@users.noreply.github.com>
2021-08-19 14:29:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,10 +11,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
     buttons = [[
-        InlineKeyboardButton('📜 Support Group', url='https://t.me/DxHelpDesk'),
-        InlineKeyboardButton('Update Channel ♻️', url='https://t.me/DX_Botz')
-    ],[
-        InlineKeyboardButton('SouceCode 💡', url='https://github.com/Jijinr/Frwdit')
+        InlineKeyboardButton('📜 Support Group', url='https://t.me/tgbotschat'),
+        InlineKeyboardButton('Update Channel ♻️', url='https://t.me/Royalbotz')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(
@@ -39,7 +37,7 @@ async def help(client, message):
 @Client.on_message(filters.private & filters.command(['about']))
 async def about(client, message):
     buttons = [[
-        InlineKeyboardButton('💡 SouceCode', url='https://github.com/Jijinr/Frwdit'),
+        InlineKeyboardButton('💡 support group', url='https://t.me/tgbotschat'),
         InlineKeyboardButton('close 🔐', callback_data='close_btn')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
